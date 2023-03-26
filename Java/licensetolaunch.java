c2295f07b66a0e4cdabf7141d7818582bf66903d
Jasonzhou97 <cheetahjason168@gmail.com>
2023-03-26 17:47:39 +0800

    new problems

--- Java/licensetolaunch.java
+++ Java/licensetolaunch.java
@@ -1,5 +1,23 @@
+import java.util.*;
+
 public class licensetolaunch {
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int least = sc.nextInt();
+        int min = 0;
+        if(n==1){
+            System.out.println(0);
+        }
+        else{
+            for(int i=0;i<n-1;i++){
+                int junk = sc.nextInt();
+                if(junk<least){
+                    least = junk;
+                    min = i+1;
+                }
+            }
+            System.out.println(min);
+        }
     }
 }
